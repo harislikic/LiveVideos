38ba8446e925d0d6be9a380fc0d4abcdbf45e8ca
indiigo-o <edin.gondzo@edu.fit.ba>
2022-08-01 16:25:55 +0200

    prettier format

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from "react";
+import React from 'react';
 import Home from './Home';
-import Videos from "./Videos";
-import Error from "./Error";
-import { Routes, Route, Link } from "react-router-dom";
-
-
+import Videos from './Videos';
+import Error from './Error';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-
-  return  (
+  return (
     <>
-    <nav>
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/videos/1" element={<Videos />} />
-        <Route path="/videos/2" element={<Error />} />
-      </Routes>
-    </nav>
-  </>
-  )
+      <nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/videos/1" element={<Videos />} />
+          <Route path="/videos/2" element={<Error />} />
+        </Routes>
+      </nav>
+    </>
+  );
 }
-
-
 
 export default App;
--- src/Home.tsx
+++ src/Home.tsx
@@ -4,11 +4,12 @@ function Home() {
   return (
     <div className="container mx-auto grid h-screen place-items-center">
       Welcome to React HLS player!
-        <div>
-          <Link to="/videos/1">Video Link</Link><br></br>
-          <Link to="/videos/2">Error Link</Link>
-        </div>
+      <div>
+        <Link to="/videos/1">Video Link</Link>
+        <br></br>
+        <Link to="/videos/2">Error Link</Link>
       </div>
+    </div>
   );
 }
 
