7b4356ee00bc33c8b86544bd9d7b7c134346793e
indiigo-o <edin.gondzo@edu.fit.ba>
2022-08-03 13:39:15 +0200

    Movie names edited

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,28 +1,28 @@
-let data = [
+const data = [
   {
     id: '1',
     link: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-    name: 'movie1',
+    name: 'Movie one',
   },
   {
     id: '2',
     link: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-    name: 'movie2',
+    name: 'Movie two',
   },
   {
     id: '3',
     link: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-    name: 'movie3',
+    name: 'Movie three',
   },
   {
     id: '4',
     link: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-    name: 'movie4',
+    name: 'Movie four',
   },
   {
     id: '5',
     link: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-    name: 'movie5',
+    name: 'Movie five',
   },
 ];
 
