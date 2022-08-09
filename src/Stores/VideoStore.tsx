20cd9ba2e5be145e2155bc249f6c3cea4ca9f803
harislikic <haris.likic@hotmail.com>
2022-08-09 13:21:38 +0200

    feat(VideoStore.tsx): added function for fetching params.

--- src/Stores/VideoStore.tsx
+++ src/Stores/VideoStore.tsx
@@ -2,6 +2,7 @@ import { makeAutoObservable } from 'mobx';
 
 class VideoStore {
   playVideo: boolean = false;
+
   constructor() {
     makeAutoObservable(this, undefined, { autoBind: true });
   }
@@ -11,5 +12,16 @@ class VideoStore {
   pause() {
     this.playVideo = false;
   }
+
+  get id() {
+    return (
+      window.location.search
+        .slice(1)
+        .split('&')
+        .find(x => x.includes('id='))
+        ?.replace('id=', '') ?? ''
+    );
+  }
 }
+
 export default VideoStore;
