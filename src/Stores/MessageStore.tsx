749b81a9ffd68e451a637c5c8186b3720a94cde8
harislikic <haris.likic@hotmail.com>
2022-08-09 13:15:41 +0200

    refactor(MessageStore.tsx): Update
    - movieParams moved inside videoStore
    -style: removed extra space and comments
    - changed types from any to apropriate type

--- src/Stores/MessageStore.tsx
+++ src/Stores/MessageStore.tsx
@@ -1,81 +1,40 @@
 import { makeAutoObservable } from 'mobx';
-
-
-// klasu ne koristim trenutno
-/*class MessageStore {
-  id: any = new Date();
-  message;
-  moveiId;
-
-  constructor(message: any, movieId: any) {
-    this.message = message;
-    this.moveiId = movieId;
-  }
-}
-*/
-
-// objekat
-/*const messageobject = {
-  id: 0,
-  message: '',
-  moveiId: '',
-
-  newMessage: function (mesage: any, movieId: any) {
-    messageobject.id = Math.floor(Math.random() * 10000);
-    messageobject.message = mesage;
-    messageobject.moveiId = movieId;
-    return messageobject;
-  },
-};
-*/
+import { rootStore } from './RootStore';
+import VideoStore from './VideoStore';
 
 class MessageStore {
-  //lista objekata
-  //listMessages: any[] = [];
+  listMessages: Record<string, string[]> = {};
+  messages: string[] = [];
 
-  listMessages: any = {};
-  messages = [];
-  movieParams: any;
-
-
-  constructor() {
+  constructor(private videoStore: VideoStore) {
     makeAutoObservable(this, undefined, { autoBind: true });
-   
-    this.movieParams = window.location.search
-    .slice(1)
-    .split('&')
-    .find(x => x.includes('id='))
-    ?.replace('id=', '') as any;
-    this.loadMessages();
-
   }
 
   addMessage(message: string) {
-    this.listMessages = JSON.parse(localStorage.getItem('message') as any);
-   
-    this.listMessages[this.movieParams].push(message);
+    this.listMessages = JSON.parse(localStorage.getItem('message') as string);
+
+    this.listMessages[this.videoStore.id].push(message);
     localStorage.setItem('message', JSON.stringify(this.listMessages));
 
-    console.log('poruka koju saljem', message);
-    console.log('diksneri data: ', this.listMessages);
   }
 
   loadMessages() {
     if (localStorage.getItem('message') == null)
+      localStorage.setItem('message', JSON.stringify(this.listMessages));
+
+    this.listMessages = JSON.parse(localStorage.getItem('message') as string);
+
+    if (!this.listMessages[this.videoStore.id] && this.videoStore.id) {
+      this.listMessages[this.videoStore.id] = this.messages;
+    }
+
     localStorage.setItem('message', JSON.stringify(this.listMessages));
-
-  this.listMessages = JSON.parse(localStorage.getItem('message') as any);
-  
-  if (!this.listMessages[this.movieParams] && this.movieParams) {
-    this.listMessages[this.movieParams] = this.messages;
   }
 
-  localStorage.setItem('message', JSON.stringify(this.listMessages) as any);
-  }
-
-  deleteMessage(message: any) {
-
-    this.listMessages[this.movieParams] = this.listMessages[this.movieParams].filter((x : any) => x != message)
+  deleteMessage(message: string) {
+    this.listMessages[this.videoStore.id] = this.listMessages[
+      this.videoStore.id
+    ].filter((x: string) => x != message);
     localStorage.setItem('message', JSON.stringify(this.listMessages));
   }
   
